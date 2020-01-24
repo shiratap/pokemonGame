6186adf840dd77d62e35b9aeacec9d486d7a5b3a
Spencer <hirataspencemama@gmail.com>
2020-01-23 17:25:29 -0800

    materialize content, multiple card additions of pokemon searches

--- public/src/index.js
+++ public/src/index.js
@@ -1,9 +1,19 @@
 $('#submit').click(() => {
 	if ($('#number').val()) {
 		$.get('https://pokeapi.co/api/v2/pokemon/' + $('#number').val(), data => {
-			$('#content').html(
-				`<h1>${data.name}</h1><img src="${data.sprites.front_default}" width="300px" height="300px"/>`
-			);
+			$('#content').prepend(`
+				<div class="col s3">
+					<div class="card">
+						<div class="card-image">
+							<img src="${data.sprites.front_default}"/>
+						</div>
+						<div class="card-content">
+							<p class="card-text">${data.name.charAt(0).toUpperCase() +
+								data.name.slice(1)}</p>
+						</div>
+					</div>
+				</div>
+				`);
 		});
 	}
 });
